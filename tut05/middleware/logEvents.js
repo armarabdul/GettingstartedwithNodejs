49e901093672488d197ff4bc9d52380f1a9e8c54
abdul muqeet <abdularmar@gmail.com>
2024-02-19 15:15:47 +0530

    basic routes added with the api creation

---
+++ tut05/middleware/logEvents.js
@@ -0,0 +1,39 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+const fs = require("fs");
+const path = require("path");
+const { log } = require("console");
+const fsPromises = require("fs").promises;
+
+const logEvent = async (message, logName) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}`;
+  console.log(logItem);
+  try {
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
+    }
+    //testing
+    await fsPromises.appendFile(
+      path.join(__dirname, "..", "logs", logName),
+      logItem + "\n"
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const logger = (req, res, next) => {
+  logEvent(`${req.method}\t ${req.headers.origin}\t ${req.url}`, "reqLog.txt");
+  console.log(`${req.method}, ${req.path}`);
+  next();
+};
+
+module.exports = { logEvent, logger };
+
+// console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
+// console.log(uuid());
+
+//learned to install dependency using npm run dev and using it in a project, and writing a script in package-json,
+//used format from date-fns package by npm i date-fns and version 4 of uuid (universal unique identifier) using npm i uuid,
